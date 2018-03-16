34c0b707079abb9fe3db6d027c946a8f83e4a56c
Chao Shin <chao.shin@gmail.com>
2018-03-16 23:07:44 +0800

    Demo 簡易說明Xcode中Object Library的Segmented Control

--- DemoObjectLibrary/ViewController.swift
+++ DemoObjectLibrary/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func mySegmentedAction(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 { //判斷使用者選擇是哪一個Segmented 0是付費App、1是免費App
+            myImageView.image = UIImage(named: "SegmentedPay") //顯示付費圖片
+        }else {
+            myImageView.image = UIImage(named: "SegmentedFree") //顯示免費圖片
+        }
+    }
+    
 }
 
